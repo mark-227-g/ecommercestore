b096edddd0a24a823bca386c3ff306e5a8cc2095
Mark Edwards <109709559+mark-227-g@users.noreply.github.com>
2023-08-07 10:42:18 -0400

    corrected for version 17 react

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import {
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css';
+
 //import Home from './pages/Home';
 
 import PuzzleCategoryPage from './pages/puzzlecategory';
